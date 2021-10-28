00d6b0053344f1064ad50280819e8f32ca89bf01
Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-10-28 16:10:33 -0300

    Creation set Alerts with sweetAlert, AlertConfirm,AlertInformation and AlertError

---
+++ src/components/Alerts/Error.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Swal from "sweetalert2";
+
+
+export default class AlertError extends Component {
+
+    constructor() {
+        super();
+        this.HandleClick = this.HandleClick.bind(this);
+    }
+
+    HandleClick() {
+        Swal.fire({
+            title: this.props.title,
+            footer: this.props.footer,
+            type: this.props.type,
+            text: this.props.text,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button class="btn btn-warning" onClick={this.HandleClick}>
+                    Alerta
+                </button>
+            </div>
+        );
+    }
+}
---
+++ src/components/Alerts/Information.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Swal from "sweetalert2";
+
+
+export default class AlertInformation extends Component {
+
+    constructor() {
+        super();
+        this.HandleClick = this.HandleClick.bind(this);
+    }
+
+    HandleClick() {
+        Swal.fire({
+            title: this.props.title,
+            footer: this.props.footer,
+            type: this.props.type,
+            text: this.props.text,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button class="btn btn-waring" onClick={this.HandleClick}>
+                    Alerta
+                </button>
+            </div>
+        );
+    }
+}
