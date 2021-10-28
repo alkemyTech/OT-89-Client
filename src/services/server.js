7bfe7b3b5c10e4b88371ce18726017e831012392
Juan Bautista Abal <keroxx3@gmail.com>
2021-10-28 03:20:21 -0300

    POST Y GET CREADOS

---
+++ src/services/server.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+url = 'localhost:3000/'
+
+export const Post = async (route, body) => {
+    const token = await localStorage.getItem('token')
+    if (token) {
+        const { data } = await axios.post(url + body, body, { withCredentials: true, headers: { 'Authorization': token } })
+        return data
+    }
+}
+export const Get = async (route) => {
+    const token = await localStorage.getItem('token')
+    if (token) {
+        const { data } = await axios.get(url + route, { withCredentials: true, headers: { 'Authorization': token } })
+        return data
+    } else {
+        return undefined
+    }
+}
