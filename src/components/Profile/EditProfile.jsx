cd1841b441673cedde0c06de623fc19384d52345
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-10-29 17:40:07 -0300

    Added Profileview
    ViewProfile function created
    DeletedProfile function created
    EditProfile function created

---
+++ src/components/Profile/EditProfile.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Formik } from 'formik';
+import { setUser } from './profileSlice';
+
+export default function EditProfile(){
+
+    const actionEdit = () => {
+        try{
+          const {data} = await axios.put('/users/' + id, );
+          dispatch(setUser(data));
+        }
+        catch(e){
+          console.log(e.response.data)
+        }
+      }
+
+    const handleSubmit = async(id) => {
+        actionEdit(id);
+        history.push('/');
+    }
+    
+    return(
+        <div>
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: ''
+                }}
+                validate={(values) => {
+                    let errors = {};
+
+                    if(!values.firstName || !values.lastName || !values.email){
+                        errors = 'Todos campos deben ser completados';
+                    }
+                    return errors;
+                }}
+                onSubmit={({resetForm}) => {                  
+                    handleSubmit();
+                    resetForm();
+                    console.log('Formulario enviado')
+                }}
+            >
+                {( {values, errors, handleSubmit, handleChange, handleBlur} ) => (
+                    <form className='form' onSubmit={handleSubmit}>
+                        {console.log(errors)}
+                        <div>
+                            <label htmlFor='firstName'>Nombre</label>
+                            <input 
+                                type='text'
+                                id='firstName'
+                                name='firstName'
+                                placeholder='John'
+                                value={values.firstName}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor='lastName'>Apellido</label>
+                            <input 
+                                type='text'
+                                name='lastName'
+                                placeholder='Doe'
+                                value={values.lastName}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor='email'>Email</label>
+                            <input 
+                                type='email'
+                                name='email'
+                                placeholder='email@email.com'
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </div>
+                        {errors && <div className='error'>{errors}</div>}
+                        <button type="submit">Enviar</button>
+                    </form>
+                )}
+            </Formik>
+        </div>
+    )
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {useDispatch, useSelector, useHistory} from 'react-redux';
+import {Link} from "react-router-dom";
+
+export default function Profile(){
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const user = useSelector((state) => state.user);
+
+    const actionDelete = () => {  
+        try{
+          await axios.delete('/users/' + id);
+          dispatch(deleteUser());
+        }
+        catch(e){
+          console.log(e.response.data)
+        }
+      }
+
+    const handleDelete = async(id) => {
+        actionDelete(id);
+        history.push('/');
+    }
+
+return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{user.firstName}</td>
+                        <td>{user.lastName}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <button onClick={() => handleDelete(user.id)}>Eliminar</button>
+                <Link to={'/profile/edit/' + id}><button>Editar</button></Link>
+            </div>
+        </div>
+    )
+}
