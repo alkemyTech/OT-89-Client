2900290f109126636ec71a63bc41cd3423136437
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-10-31 20:36:35 -0300

    .scss file added - styles added to editPrifile component

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { setUser } from './profileSlice';
 import { useDispatch, useHistory } from 'react-redux';
+import './editProfile.scss';
 
 export default function EditProfile() {
     
@@ -25,97 +26,89 @@ export default function EditProfile() {
   };
 
   return (
-    <div>
+    <div className='form-container'>
       <Formik
         initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
+          firstName: '',
+          lastName: '',
+          email: ''
         }}
         validate={(valuesInfo) => {
           let err = {};
 
           //Valido que completen el campo con nombre
-          if (!valuesInfo.firstName) {
-            err.firstName = "Por favor ingrese un nombre";
+          if(!valuesInfo.firstName) {
+            err.firstName = 'Por favor ingrese un nombre';
 
-            //Valido con una expresión regular que el nombre solo contenga letras y espacios
-          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valuesInfo.firstName)) {
-            err.firstName = "El nombre solo puede contener letras y espacios";
+          //Valido con una expresión regular que el nombre solo contenga letras y espacios
+          }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valuesInfo.firstName)) {
+            err.firstName = 'El nombre solo puede contener letras y espacios';
           }
-          if (!valuesInfo.lastName) {
-            err.lastName = "Por favor ingrese un apellido";
-          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valuesInfo.lastName)) {
-            err.lastName = "El apellido solo puede contener letras y espacios";
+          if(!valuesInfo.lastName) {
+            err.lastName = 'Por favor ingrese un apellido';
+          }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valuesInfo.lastName)) {
+            err.lastName = 'El apellido solo puede contener letras y espacios';
           }
-          if (!valuesInfo.email) {
-            err.email = "Por favor ingrese un email";
-          } else if (
-            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
-              valuesInfo.email
-            )
-          ) {
-            err.email =
-              "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo";
+          if(!valuesInfo.email) {
+            err.email = 'Por favor ingrese un email';
+          }else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valuesInfo.email)) {
+            err.email = 'El correo solo puede contener letras, numeros, puntos, guiones y guion bajo';
           }
 
           return err;
         }}
-        onSubmit={(valuesInfo, { resetForm }) => {
-          handleSubmit(valuesInfo);
-          changeSubmitedForm(true);
-          setTimeout(() => changeSubmitedForm(false), 5000);
-          resetForm();
-        }}
-      >
-        {({ errors }) => (
-          <Form className="form">
-            <div>
-              <label htmlFor="firstName">Nombre</label>
-              <Field
-                type="text"
-                id="firstName"
-                name="firstName"
-                placeholder="Jhon"
-              />
+        onSubmit={(valuesInfo, {resetForm}) =>{
+        handleSubmit(valuesInfo);
+        changeSubmitedForm(true);
+        setTimeout(() => changeSubmitedForm(false), 5000);
+        resetForm();
+        console.log(valuesInfo)
+      }}>
+        {( {errors} ) => (
+          <Form className='form'>
+            <div className = 'input-container'>
+              <label htmlFor='firstName'>Nombre</label>
+              <Field 
+                className='form-control'
+                type='text' 
+                id='firstName' 
+                name='firstName' 
+                placeholder='Jhon'/>
 
-              <ErrorMessage
-                name="firstName"
-                component={() => <div className="err">{errors.firstName}</div>}
-              />
+                <ErrorMessage name='firstName' component={() => (
+                  <small className='err'>{errors.firstName}</small>
+                )}/>
+            </div>
+            <div className = 'input-container'>
+              <label htmlFor='lastName'>Apellido</label>
+              <Field 
+                className='form-control'
+                type='text' 
+                id='lastName' 
+                name='lastName' 
+                placeholder='Doe'/>
+
+                <ErrorMessage name='lastName' component={() => (
+                  <small className='err'>{errors.lastName}</small>
+                )}/>
             </div>
             <div>
-              <label htmlFor="lastName">Apellido</label>
-              <Field
-                type="text"
-                id="lastName"
-                name="lastName"
-                placeholder="Doe"
-              />
+              <label htmlFor='email'>Email</label>
+              <Field 
+                className='form-control'
+                type='email' 
+                id='email' 
+                name='email' 
+                placeholder='correo@correo.com'/>
 
-              <ErrorMessage
-                name="lastName"
-                component={() => <div className="err">{errors.lastName}</div>}
-              />
+                <ErrorMessage name='email' component={() => (
+                  <small className='err'>{errors.email}</small>
+                )}/>
             </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <Field
-                type="email"
-                id="email"
-                name="email"
-                placeholder="correo@correo.com"
-              />
-
-              <ErrorMessage
-                name="email"
-                component={() => <div className="err">{errors.email}</div>}
-              />
+            <div className='button-container'>
+              <button className='btn btn-primary' type='submit'>Enviar</button>
             </div>
-            <button type="submit">Enviar</button>
-            {submitedForm && (
-              <p className="success">Formulario enviado con exito!</p>
-            )}
+            {submitedForm && <small className="success">Formulario enviado con exito!</small>}
           </Form>
         )}
       </Formik>
