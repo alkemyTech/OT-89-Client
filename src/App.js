782a72420e60e3e6965419a2bd004f683a0866f6
Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-11-01 09:21:32 -0300

    Add component to the App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react';import './App.css';
+import { Alert } from './components/Alerts/Alert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+
+export default function App() { //Ambas alertas deben se llamadas en un botón
+  <Alert type={'success'} ttitle={'Error'} icon={'info'} buttonName={'Login'} text={'Esto es información'} button={'info'}/>
+        
 }
-
-export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
+
+const store = configureStore({
+  reducer,
+  devTools: process.env.NODE_ENV !== "production"
+});
+
+export default store;
+
 export default configureStore({
   reducer: {
     counter: counterReducer,
