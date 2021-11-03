8daa26d73ac1d8ea763b5805061660239ad19968
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2021-11-03 08:37:47 -0300

    bugs fixed again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { Home } from './Pages/Home';
-
-function App() {
-  return ( <Home/>
-  );
-}
-
-export default App;
