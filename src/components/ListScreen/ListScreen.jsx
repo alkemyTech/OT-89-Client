5a5be90047b86a47927de27ab6c261eff3d811cc
Chamarez <michel.chamarez@gmail.com>
2021-11-04 02:21:55 +0100

    ticket done, todo: modularice and styles

---
+++ src/components/ListScreen/ListScreen.jsx
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import "./ListScreen.scss";
+
+const ListScreen = () => {
+  const dataReceived = [
+    { id: 1, name: "prueba1", image: "url", createdAt: "24-03-2012" },
+    { id: 2, name: "prueba2", image: "url", createdAt: "24-03-2012" },
+    { id: 3, name: "prueba3", image: "url", createdAt: "24-03-2012" },
+    { id: 4, name: "prueba4", image: "url", createdAt: "24-03-2012" },
+  ];
+
+  const [data, setData] = useState(dataReceived);
+  const [modalEdit, setModalEdit] = useState(false);
+  const [modalDelete, setModalDelete] = useState(false);
+  const [selected, setSelected] = useState({
+    id: "",
+    name: "",
+    image: "",
+    createdAt: "",
+  });
+  let toEdit = { id: "", name: "", image: "", createdAt: "" };
+  let toDelete = { id: "" };
+
+  const selectList = (element, cas) => {
+    setSelected(element);
+    cas === "Edit" ? setModalEdit(true) : setModalDelete(true);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSelected((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    /*     console.log(selected); */
+  };
+
+  const editar = () => {
+    var newData = data;
+    newData.map((list) => {
+      if (list.id === selected.id) {
+        list.name = selected.name;
+        list.image = selected.image;
+        list = toEdit;
+        return toEdit;
+      }
+    });
+    /*     setData(newData); */
+    setModalEdit(false);
+    console.log(toEdit);
+  };
+
+  const del = () => {
+    setData(
+      data.filter((dat) => dat.id !== selected.id, (toDelete = selected.id))
+    );
+    setModalDelete(false);
+    return toDelete;
+  };
+
+  return (
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Image</th>
+            <th>CreatedAt</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((element) => (
+            <tr>
+              <td>{element.name}</td>
+              <td>{element.image}</td>
+              <td>{element.createdAt}</td>
+              <td>
+                <button
+                  className="btn btn-primary btn-lg"
+                  onClick={() => selectList(element, "Edit")}
+                >
+                  editar
+                </button>
+                {"  "}
+                <button
+                  className="btn btn-danger btn-lg"
+                  onClick={() => selectList(element, "Delete")}
+                >
+                  eliminar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal isOpen={modalEdit}>
+        <ModalHeader>
+          <div>
+            <h3>Editar</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              name="id"
+              value={selected && selected.id}
+            />
+            <br />
+
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              value={selected && selected.name}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Image</label>
+            <input
+              className="form-control"
+              type="text"
+              name="image"
+              value={selected && selected.image}
+              onChange={handleChange}
+            />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => editar()}>
+            Actualizar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => setModalEdit(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalDelete}>
+        <ModalBody>
+          Estás Seguro que deseas eliminar el país {selected && selected.name}
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={() => del()}>
+            Sí
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setModalDelete(false)}
+          >
+            No
+          </button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default ListScreen;
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,42 +1,11 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Navbar} from '../components/Navbar';
-import {Footer} from '../components/Footer';
-import {Home} from '../components/Home';
-import {About} from '../components/About';
-import {Activities} from '../components/Activities';
-import {Novelties} from '../components/Novelties';
-import {News} from '../components/News';
-import {Testimonials} from '../components/Testimonials';
-import {Contacts} from '../components/Contacts';
-import {Contribute} from '../components/Contribute';
-import {Login} from '../components/Login';
-import {Register} from '../components/Register';
-import {Profile} from '../components/Profile';
-import {NotFound} from '../component/NotFound';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListScreen from "../components/ListScreen/ListScreen";
 
 export const AppRouter = () => {
-    return(
-        <Router>
-            <Navbar />
-                <Switch>
-                    <Route exact path = '/' component = {Home}/>
-                    {/* nosotros = about */}
-                    <Route exact path = '/about' component = {About}/>
-                    <Route exact path = '/activities' component = {Activities}/>
-                    {/* novedades = novelties */}
-                    <Route exact path = '/novelties' component = {Novelties}/>
-                    {/* noticias = news */}
-                    <Route exact path = '/news' component = {News}/>
-                    <Route exact path = '/testimonials' component = {Testimonials}/>
-                    <Route exact path = '/contacts' component = {Contacts}/>
-                    <Route exact path = '/contribute' component = {Contribute}/>
-                    <Route exact path = '/login' component = {Login}/>
-                    <Route exact path = '/register' component = {Register}/>
-                    <Route exact path = '/profile' component = {Profile}/>
-                    <Route exact path = '*' component = {NotFound}/>
-                </Switch>
-            <Footer />
-        </Router>
-    )
-}
+  return (
+    <>
+      <ListScreen />
+    </>
+  );
+};
