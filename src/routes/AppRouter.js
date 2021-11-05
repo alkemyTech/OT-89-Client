0dbf54ccac6a004c72f5ec20086baae95bf01a63
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2021-11-05 09:23:42 -0300

    Update AppRouter.js

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,43 +1,45 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-/* import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
-import { Home } from "../components/Home";
-import { About } from "../components/About";
-import { Activities } from "../components/Activities";
-import { Novelties } from "../components/Novelties";
-import { News } from "../components/News";
-import { Testimonials } from "../components/Testimonials";
-import { Contacts } from "../components/Contacts";
-import { Contribute } from "../components/Contribute";
-import { Login } from "../components/Login";
-import { Register } from "../components/Register";
-import { Profile } from "../components/Profile";
-import { NotFound } from "../component/NotFound"; */
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Activities } from "../pages/Activities";
+import { Novelties } from "../pages/Novelties";
+import { NoveltiesIndividual } from '../pages/NoveltiesIndividual'
+import { News } from "../pages/News";
+import { Testimonials } from "../pages/Testimonials";
+import { Contacts } from "../pages/Contacts";
+import { Contribute } from "../pages/Contribute";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+// import { Profile } from "../pages/Profile";
+import { NotFound } from "../pages/NotFound";
 
 export const AppRouter = () => {
   return (
     <Router>
-      {/*   <Navbar /> */}
-      {/*   <Switch> */}
-      {/*   <Route exact path="/" component={Home} / */}>
-      {/* nosotros = about */}
-      {/*  <Route exact path="/about" component={About} /> */}
-      {/*     <Route exact path="/activities" component={Activities} /> */}
-      {/* novedades = novelties */}
-      {/* <Route exact path="/novelties" component={Novelties} /> */}
-      {/* noticias = news */}
-      {/* <Route exact path="/news" component={News} /> */}
-      {/* <Route exact path="/testimonials" component={Testimonials} /> */}
-      {/* <Route exact path="/contacts" component={Contacts} /> */}
-      {/* <Route exact path="/contribute" component={Contribute} /> */}
-      {/* <Route exact path="/login" component={Login} /> */}
-      {/* <Route exact path="/register" component={Register} /> */}
-      {/* <Route exact path="/profile" component={Profile} /> */}
-      {/* <Route exact path="*" component={NotFound} /> */}
-      {/*     </Switch> */}
-      {/*  <Footer /> */}
+        <Header />
+            <Switch>
+            <Route exact path="/" component={Home}/> 
+            // /nosotros = about
+            <Route exact path="/about" component={About} />
+            <Route exact path="/activities" component={Activities} />
+            {/* novedades = novelties */}
+            <Route exact path="/novelties" component={Novelties} />
+            <Route exact path="/novelties/:id" component = {NoveltiesIndividual}/>
+            {/* noticias = news */}
+            <Route exact path="/news" component={News} />
+            <Route exact path="/testimonials" component={Testimonials} />
+            <Route exact path="/contacts" component={Contacts} />
+            <Route exact path="/contribute" component={Contribute} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            {/* <Route exact path="/profile" component={Profile} /> */}
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+       <Footer />
     </Router>
   );
 };
