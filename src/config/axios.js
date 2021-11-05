4306cdd4123ddab5e16857e4ac8c757ddfb4cbf2
Luciano Ibarra <luciano39340268@gmail.com>
2021-11-05 09:22:52 -0300

    Delete axios.js

--- src/config/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const clienteAxios = axios.create({
-  baseURL: 'http://localhost:3000'  /* Insertar la ruta del endpoint del backend */
-})
-
-export default clienteAxios;
