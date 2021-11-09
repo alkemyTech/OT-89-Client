706f5029f3d831573732e80d8d4e505a0ea0b71d
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-09 17:33:57 -0300

    Changed routes and added conexion to server

--- src/pages/backoffice/ListUsers/ListUsers.jsx
+++ src/pages/backoffice/ListUsers/ListUsers.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import apiService from '../../../services/server';
 
-export default function ListUsers() {
+export const ListUsers = () => {
 
-    
-
+    React.useEffect(async () => {
+        try{
+            const users = await apiService.get("/users/");
+        }
+        catch(e){
+            console.log(e.response.data);
+        }
+    }, []);
 
     return(
         <div>
