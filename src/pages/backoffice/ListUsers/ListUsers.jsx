ae567e3b02c3e193d3b9d2cb5d669fedecc36331
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-09 18:35:30 -0300

    Added users API for testing users list render and table for visualization

--- src/pages/backoffice/ListUsers/ListUsers.jsx
+++ src/pages/backoffice/ListUsers/ListUsers.jsx
@@ -1,20 +1,84 @@
-import React from 'react';
-import apiService from '../../../services/server';
+import React from "react";
+import apiService from "../../../services/server";
 
 export const ListUsers = () => {
 
-    React.useEffect(async () => {
-        try{
-            const users = await apiService.get("/users/");
-        }
-        catch(e){
-            console.log(e.response.data);
-        }
-    }, []);
+    //API para probar renderizado de usuarios
+  const usersArray = [
+    {
+        id: "1",
+      firstName: "Leanne Graham",
+      lastName: "Bret",
+      email: "sincere@april.biz",
+    },
+    {
+        id: "2",
+      firstName: "Ignacio",
+      lastName: "Garcia",
+      email: "ig.ignacio@gmail.com",
+    },
+    {
+        id: "3",
+      firstName: "Mirna Ayelen Graham",
+      lastName: "Garcia",
+      email: "mirnaayelen@gmail.com",
+    },
+  ];
 
-    return(
-        <div>
+  React.useEffect(async () => {
+    try {
+      const users = await apiService.get("/users");
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  }, []);
 
-        </div>
-    );
+  //FUNCION PARA ELIMINAR USUARIO:
+
+  // const handleDelete = async (idAEliminar) => {
+  //     try{
+  //         await apiService.delete("/users/" + idAEliminar);
+  //     }
+  //     catch(e){
+  //         console.log(e.response.data);
+  //     }
+  // };
+
+  return (
+    <>
+      <div>
+          <h1>Listado de usuarios:</h1>
+      </div>
+      <div>
+          <table>
+              <thead>
+                  <tr>
+                      <th>Nombre</th>
+                      <th>Apellido</th>
+                      <th>Email</th>
+                      <th></th>
+                      <th></th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {usersArray.map((oneUser) => 
+                    oneUser ? (
+                      <tr key={oneUser.id}>
+                          <td>{oneUser.firstName}</td>
+                          <td>{oneUser.lastName}</td>
+                          <td>{oneUser.email}</td>
+                          <td>
+                              <button>Editar</button>
+                          </td>
+                          <td>
+                              <button>Eliminar</button>
+                          </td>
+                      </tr>
+                    ) : null
+                  )}
+              </tbody>
+          </table>
+      </div>
+    </>
+  );
 };
