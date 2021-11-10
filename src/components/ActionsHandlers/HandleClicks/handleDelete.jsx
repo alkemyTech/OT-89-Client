d7f27f78b77e4a379f054387b709389de8a525f6
Juan Bautista Abal <keroxx3@gmail.com>
2021-11-09 22:42:52 -0300

    eliminar novelties identico a eliminar categorias

--- src/components/ActionsHandlers/HandleClicks/handleDelete.jsx
+++ src/components/ActionsHandlers/HandleClicks/handleDelete.jsx
@@ -2,6 +2,7 @@ import apiService from "../../../services/server"
 import { useDispatch } from 'react-redux'
 import { Confirm } from "../../Alert/Alert"
 import { deleteCategory } from "../../../features/slices/categoriesSlice"
+import { deleteNovelty } from "../../../features/slices/noveltySlice"
 
 export const HandleDeleteCategory = async (id) => {
     const dispatch = useDispatch()
@@ -10,6 +11,16 @@ export const HandleDeleteCategory = async (id) => {
         const deleteResult = await apiService.delete("/categories", { id })
         if (deleteResult.data.message === "¡Category deleted successfully!") {
             dispatch(deleteCategory(id))
+        }
+    }
+}
+export const HandleDeleteNovelty = async (id) => {
+    const dispatch = useDispatch()
+    const alertResult = await Confirm("Eliminar novedad", "Esta intentando eliminar una novedad, ¿desea continuar?")
+    if (alertResult) {
+        const deleteResult = await apiService.delete("/categories", { id })
+        if (deleteResult.data.message === "¡Novelty deleted successfully!") {
+            dispatch(deleteNovelty(id))
         }
     }
 }
