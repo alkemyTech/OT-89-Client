4e61e6a6934ec2dd828cbe797cde0fcef59a074c
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-10 20:51:20 -0300

    Added conexion to novelties/id endpoint

--- src/components/Novelty/Novelty.js
+++ src/components/Novelty/Novelty.js
@@ -1,76 +1,40 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
-import { fakedata } from '../Card/infofake'
 import { Button } from '../utils/buttons/Button';
 import './Novelty.scss'
+import apiService from "../../services/server";
+import { Alert } from "../Alert/Alert";
 
-export const Novelty = () => {
-    
-    const localization = useLocation()
+export const Novelty = ({id}) => {
 
-    const [idnovelty, setIdNovelty] = useState(0)
     const [ data, setData ] = useState({
         id: 0,
         title: "",
         description: [],
         image: ""
     }) 
-
     useEffect(() => {
-        const id = () => {
-            const id =  localization.pathname.split("/", 3)
-            setIdNovelty(parseInt(id[2]))
+
+        const getData = async () => {
+            try{
+                const response = await apiService.get(`/novelties/${id}`);
+            
+                setData(response);
+            }
+            catch(e){
+                Alert("Error", e, "error");
+            }
         }
-        id()
+        getData();      
     }, [])
 
-    useEffect(() => {
-
-        const noveltyFinderFake = () => { //FAKE 
-            const item = fakedata.find(element => element.id === idnovelty)
-            const { title, description, image } = item
-            const descFiltered = description.split("\n", -1)
-            setData({
-                id: idnovelty,
-                title: title,
-                description: descFiltered,
-                image: image
-            })
-        }
-
-        // const noveltyFinder = async () => {
-        // try {
-        //     const res = await axios.get('/novelties/:id')
-        //     const  { title, description, image } = await res.data
-        //     setData({
-        //         id: idnovelty,
-        //         title: title,
-        //         description: description,
-        //         image: image                
-        //     })
-        //     } catch (error) {
-        //         console.log(error)
-        //     }    
-        // }
-
-        if(idnovelty !== 0) { 
-            noveltyFinderFake()
-        } else {
-            return
-        }
-    }, [idnovelty])
-
-
-    return (
+    return (      
         <div className="Novelty">
             <img className="Novelty__img" src={ data.image} alt={data.title}/>
             <div className="Novelty__description">
                 <h2>{data.title}</h2>
-                {data.description.map(pgr => (
-                    <p>{pgr}</p>
-                ))}
+                <h3>{data.description}</h3>
                 <div className="buttons">
-                    <Button className="button button-primary" title="Volver a novedades" url={`/novelties`}/>
+                    <Button className="button button-primary" title="Volver a novedades" url="/novelties"/>
                 </div>
             </div>            
         </div>
