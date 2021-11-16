cb540659ba412a43b75ce5b561d61ed6d2b98257
Martin Cappi <mcappi91@gmail.com>
2021-11-16 13:15:21 -0300

    categories navigation

---
+++ src/pages/backoffice/categories/CategoriesScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CategoriesScreen = () => {
+    return (
+        <div>
+            <p>Title</p>
+            <p>add category</p>
+            <p>List</p>
+        </div>
+    )
+}
+
+export default CategoriesScreen
