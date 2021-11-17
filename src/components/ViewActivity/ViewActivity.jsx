721b5a15bbbe8957d9d9e61a69fc6bfd9b23f336
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-17 16:11:25 -0300

    imported utils and created initial function

---
+++ src/components/ViewActivity/ViewActivity.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import apiService from "../../services/server";
+import { Alert } from "../Alert/Alert";
+import { Button } from "../utils/buttons/Button";
+
+//Activity details
+//Faltan implementar estilos
+export const Activity = ({id}) => {
+
+
+}
