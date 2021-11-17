d27a12e333604d0bad1fabda7e5f508cafd391c0
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-17 16:12:47 -0300

    Added initial state and conexion to server

--- src/components/ViewActivity/ViewActivity.jsx
+++ src/components/ViewActivity/ViewActivity.jsx
@@ -7,5 +7,33 @@ import { Button } from "../utils/buttons/Button";
 //Faltan implementar estilos
 export const Activity = ({id}) => {
 
+    const [ data, setData ] = useState({
+        id: 0,
+        name: "",
+        image: "",
+        content: []
+    }) 
+    useEffect(() => {
+        const getData = async () => {
+            try{
+                const response = await apiService.get("/activities/" + id);
+            
+                if(!response){
+                    Alert("Error", "La actividad que intenta visualizar no existe en la base de datos", "error");
+                }else{
+                    setData(response.data.data);
+                }
+            }
+            catch(e){
+                console.log(e.response.data);
+            }
+        }
+        getData();      
+    }, [])
 
+    return (      
+        <div>
+           
+        </div>
+    )
 }
