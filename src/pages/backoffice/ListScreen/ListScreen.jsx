87f29acec6cceca0f794b943642f817209984349
Martin Cappi <mcappi91@gmail.com>
2021-11-19 20:39:10 -0300

    split forms into different modals
    work in progress

--- src/pages/backoffice/ListScreen/ListScreen.jsx
+++ src/pages/backoffice/ListScreen/ListScreen.jsx
@@ -7,62 +7,41 @@ import apiService from "../../../services/server";
 import "./ListScreen.scss";
 import Novedad from "../novedades/newNovelities";
 
-
-
-
-const ListScreen = (props) => {
-  const dispatch = useDispatch()
-  const HandleDeleteNovelty = async (id) => {
-    const alertResult = await Confirm("Eliminar novedad", "Esta intentando eliminar una novedad, ¿desea continuar?")
-    if (alertResult) {
-      setData(data.filter((item) => item.id !== id));
-      const deleteResult = await apiService.delete("/categories", { id })
-      console.log(deleteResult)
-      if (deleteResult.data.message === "¡Novelty deleted successfully!") {
-        dispatch(deleteNovelty(id))
-      }
-    }
-  }
-
-  const [data, setData] = useState([
-    {
-      id: 1,
-      name: "prueba1",
-      image: "https://getbootstrap.com/",
-      createdAt: "24-03-2012",
-    },
-    {
-      id: 2,
-      name: "prueba2",
-      image: "https://getbootstrap.com/",
-      createdAt: "24-03-2012",
-    },
-    {
-      id: 3,
-      name: "prueba3",
-      image: "uhttps://getbootstrap.com/rl",
-      createdAt: "24-03-2012",
-    },
-    {
-      id: 4,
-      name: "prueba4",
-      image: "https://getbootstrap.com/",
-      createdAt: "24-03-2012",
-    },
-  ]);
+const ListScreen = () => {
+  const [data, setData] = useState(sampleNovelties);
   const [modalEdit, setModalEdit] = useState(false);
+  const [modalAdd, setModalAdd] = useState(false);
   const [selected, setSelected] = useState({
     id: "",
     name: "",
     image: "",
     createdAt: "",
   });
+
+  const dispatch = useDispatch();
+
+  const HandleDeleteNovelty = async (id) => {
+    const alertResult = await Confirm(
+      "Eliminar novedad",
+      "Esta intentando eliminar una novedad, ¿desea continuar?"
+    );
+    if (alertResult) {
+      setData(data.filter((item) => item.id !== id));
+      const deleteResult = await apiService.delete("/categories", { id });
+      //console.log(deleteResult);
+      if (deleteResult.data.message === "¡Novelty deleted successfully!") {
+        dispatch(deleteNovelty(id));
+      }
+    }
+  };
+
   let toEdit = { id: "", name: "", image: "", createdAt: "" };
 
   const handleEdit = (element) => {
     setSelected(element);
-    setModalEdit(true)
-  }
+    setModalEdit(true);
+  };
+  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setSelected((prevState) => ({
@@ -87,97 +66,160 @@ const ListScreen = (props) => {
   };
 
   return (
-    <div className="container">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Titulo</th>
-            <th>Image</th>
-            <th>CreatedAt</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((element) => (
-            <tr key={element.id}>
-              <td>{element.name}</td>
-              <td>{element.image}</td>
-              <td>{element.createdAt}</td>
-              <td>
-                <button
-                  className="btn primary"
-                  onClick={() => {handleEdit(element)}}
-                >
-                  editar
-                </button>
-                {"  "}
-                <button
-                  className=" btn danger btn-danger "
-                  onClick={() => { HandleDeleteNovelty(element.id) }}
-                >
-                  eliminar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="container">
-        <Modal isOpen={modalEdit}>
-          <ModalHeader>
-            <div>
-              <h3>Editar</h3>
-            </div>
-          </ModalHeader>
-          <ModalBody>
-               <Novedad/>
-          </ModalBody> 
-            <div className="form-group">
-              <label>ID</label>
-              <input
-                className="form-control"
-                readOnly
-                type="text"
-                name="id"
-                value={selected.id}
-              />
-              <br />
-
-              <label>Name</label>
-              <input
-                className="form-control"
-                type="text"
-                name="name"
-                value={selected && selected.name}
-                onChange={handleChange}
-              />
-              <br />
-
-              <label>Image</label>
-              <input
-                className="form-control"
-                type="text"
-                name="image"
-                value={selected && selected.image}
-                onChange={handleChange}
-              />
-              <br />
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <button className="btn primary" onClick={() => editar()}>
-              Actualizar
-            </button>
-            <button
-              className="btn danger btn-danger"
-              onClick={() => setModalEdit(false)}
-            >
-              Cancelar
-            </button>
-          </ModalFooter>
-        </Modal>
+    <>
+      <div className="heading">
+        <h1>Novedades</h1>
+        {/*Este H1 se debe renderizar segun las lista que contenga -- nepha: wtf?? */}
+        <button
+          className="button primary"
+          onClick={() => {
+            setModalAdd(true);
+          }}
+        >
+          Agregar Novedades
+        </button>
       </div>
-    </div>
+
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Titulo</th>
+              <th>Image</th>
+              <th>CreatedAt</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((element) => (
+              <tr key={element.id}>
+                <td>{element.name}</td>
+                <td>{element.image}</td>
+                <td>{element.createdAt}</td>
+                <td>
+                  <button
+                    className="btn primary"
+                    onClick={() => {
+                      handleEdit(element);
+                    }}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className=" btn danger btn-danger "
+                    onClick={() => {
+                      HandleDeleteNovelty(element.id);
+                    }}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div
+          className="container"
+          onClick={() => {
+            setModalAdd(false);
+            setModalEdit(false);
+          }}
+        >
+          <Modal isOpen={modalAdd} backdrop={true}>
+            <ModalHeader>
+              <div>
+                <h3>Agregar una nueva Novedad</h3>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <Novedad setModalAdd={setModalAdd} id={null} />
+            </ModalBody>
+            <ModalFooter></ModalFooter>
+          </Modal>
+
+          {/* Separacion de Modales */}
+
+          <Modal isOpen={modalEdit}>
+            <ModalHeader>
+              <div>
+                <h3>Editar</h3>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <div className="form-group">
+                <label>ID</label>
+                <input
+                  className="form-control"
+                  readOnly
+                  type="text"
+                  name="id"
+                  value={selected.id}
+                />
+                <br />
+
+                <label>Name</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="name"
+                  value={selected && selected.name}
+                  onChange={handleChange}
+                />
+                <br />
+
+                <label>Image</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="image"
+                  value={selected && selected.image}
+                  onChange={handleChange}
+                />
+                <br />
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <button className="btn primary" onClick={() => editar()}>
+                Actualizar
+              </button>
+              <button
+                className="btn danger btn-danger"
+                onClick={() => setModalEdit(false)}
+              >
+                Cancelar
+              </button>
+            </ModalFooter>
+          </Modal>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default ListScreen;
+
+const sampleNovelties = [
+  {
+    id: 1,
+    name: "prueba1",
+    image: "https://getbootstrap.com/",
+    createdAt: "24-03-2012",
+  },
+  {
+    id: 2,
+    name: "prueba2",
+    image: "https://getbootstrap.com/",
+    createdAt: "24-03-2012",
+  },
+  {
+    id: 3,
+    name: "prueba3",
+    image: "uhttps://getbootstrap.com/rl",
+    createdAt: "24-03-2012",
+  },
+  {
+    id: 4,
+    name: "prueba4",
+    image: "https://getbootstrap.com/",
+    createdAt: "24-03-2012",
+  },
+];
--- src/pages/backoffice/novedades/newNovelities.js
+++ src/pages/backoffice/novedades/newNovelities.js
@@ -1,121 +1,132 @@
 import React from "react";
-import {CKEditor} from '@ckeditor/ckeditor5-react'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import apiService from "../../../services/server";
 
-export default class Novedad extends React.Component{
-
+export default class Novedad extends React.Component {
   state = {
     name: "",
     image: "",
     content: "",
-    categoryId: ""
-  }
+    categoryId: "",
+    id: this.props.id || null,
+  };
 
   handleChange = (event) => {
-      const target = event.target
-      const {
-        name,
-        value
-      } = target
+    const target = event.target;
+    const { name, value } = target;
 
-      this.setState({
-        [name]: value
-      })
-}
-handleCkeditorState = (event, editor) => {
-  const data = editor.getData()
-  this.setState({
-    content: data
-  })
-  console.log(data)
-
-}
-  handleSubmit = (values) => {
-   const FormNovelities = {
-     name: values.name,
-     image: values.image,
-     content: values.editor,
-     categoryId: values.categoryId
-   };
- 
-  console.log("novedades", FormNovelities)
-  apiService.post("/news", FormNovelities) /* Cambiar ruta segun corresponda*/
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((error) => {
-      console.log(error); /* Se debe importar el alert y pasar el error */
+    this.setState({
+      [name]: value,
     });
-}; 
+  };
 
-    render(){ 
-        return(            
-<div className="container">  
-  <div className="row">
-    <div className=" col-lg-12 col-md-12 col-xs-12">     
-        <form className="mt-3">
-          <div className="form-group mb-3">
-            <label htmlFor="title">
-              Titulo:
-              <input
-                type="text"
-                className="form-control"
-                value={this.state.name}
-                onChange={this.handleChange}
-                name="name"
-                id="name"
-                required
-              />             
-            </label>
+  handleCkeditorState = (event, editor) => {
+    const data = editor.getData();
+    this.setState({
+      content: data,
+    });
+    console.log(data);
+  };
+
+  handleSubmit = (event) => {
+    const FormNovelities = {
+      name: this.state.name,
+      image: this.state.image,
+      content: this.state.content,
+      categoryId: this.state.categoryId,
+    };
+
+    console.log("novedades", FormNovelities);
+    apiService
+      .post("/news", FormNovelities) /* Cambiar ruta segun corresponda*/
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error); /* Se debe importar el alert y pasar el error */
+      });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.id ? undefined : (
+          <div className="container">
+            <div className="row">
+              <div className=" col-lg-12 col-md-12 col-xs-12">
+                <form className="mt-3" onSubmit={(e) => e.preventDefault()}>
+                  <div className="form-group mb-3">
+                    <label htmlFor="title">
+                      Titulo:
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        name="name"
+                        id="name"
+                        required
+                      />
+                    </label>
+                  </div>
+                  <div className="form-group mb-3">
+                    <label htmlFor="image">
+                      Imagen:
+                      <input
+                        type="file"
+                        className="form-control"
+                        name="image"
+                        id="image"
+                        value={this.state.image}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </label>
+                  </div>
+                  <div className="form-group mb-3">
+                    <CKEditor
+                      editor={ClassicEditor}
+                      data=""
+                      onReady={(editor) => {}}
+                      name="content"
+                      onChange={this.handleCkeditorState}
+                    />
+                  </div>
+                  <div className="form-group mb-3">
+                    <label htmlFor="category">
+                      categoria:
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="categoryId"
+                        id="categoryId"
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </label>
+                  </div>
+                  <div className="form-group my-3">
+                    <button
+                      type="submit"
+                      className="btn btn-primary"
+                      onClick={this.handleSubmit}
+                    >
+                      Agregar
+                    </button>
+                    <button
+                      className="btn danger btn-danger"
+                      onClick={() => this.props.setModalAdd(false)}
+                    >
+                      Cancelar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-          <div className="form-group mb-3">
-            <label htmlFor="image">
-              Imagen:
-              <input
-                type="file"
-                className="form-control"
-                name="image"
-                id="image"
-                value={this.state.image}
-                onChange={this.handleChange}
-                required />              
-            </label>
-          </div>
-          <div className="form-group mb-3">           
-          <CKEditor
-           editor={ ClassicEditor }
-                    data=""
-                    onReady={ editor => {
-                        
-                    } }
-                    name="content"                 
-                    onChange={this.handleCkeditorState}
-                    
-                />            
-          </div> 
-          <div className="form-group mb-3">
-            <label htmlFor="category">
-              categoria:
-              <input
-                type="text"
-                className="form-control"
-                name="categoryId"
-                id="categoryId"                
-                onChange={this.handleChange}
-                required                
-              />             
-            </label>
-          </div>        
-          <div className="form-group my-3">
-            <button type="submit" className="btn btn-primary" onClick={this.handleSubmit} >
-              Agregar
-            </button>
-          </div>          
-        </form>      
-    </div>
-  </div>
-</div>      
-        )
-    }
+        )}
+      </>
+    );
+  }
 }
