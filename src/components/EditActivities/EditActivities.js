a912d328a2e573ec940dd95f197c5cc669281a6c
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2021-11-19 12:43:35 -0300

    final changes

--- src/components/EditActivities/EditActivities.js
+++ src/components/EditActivities/EditActivities.js
@@ -8,11 +8,19 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "./EditActivities.scss";
 
 const EditActivities = ({ actId = 0 }) => {
+  const [visible, setVisible] = useState(false);
+
   const [data, setData] = useState({
     actname: "",
     file: "",
     content: "",
   });
+
+  const handlerchange = (editor) => {
+    const data = editor.getData();
+    console.log(data);
+    // setData({ ...data, content: data });
+  };
 
   // Peticion de get en caso de que id !== 0
   useEffect(() => {
@@ -20,13 +28,8 @@ const EditActivities = ({ actId = 0 }) => {
       const res = await apiService.get(`/activities/${actId}`);
       const { data } = await res.data;
       if (data.length === 0) {
+        setData(data);
       } else {
-        setData({
-          name: "Hola mundo",
-          file: "image.jpg",
-          content:
-            "Este contrenido es contenidoso extremadamente contenidanhios",
-        });
         const { message } = await res.data;
         Alert("error", message, "error", 3000);
       }
@@ -40,6 +43,7 @@ const EditActivities = ({ actId = 0 }) => {
 
   //Peticiones a la base de datos
   const handlerSubmit = async (values) => {
+    console.log(values);
     if (actId !== 0) {
       // Creacion de actividades
       const res = await apiService.post("/actividades", values);
@@ -67,110 +71,75 @@ const EditActivities = ({ actId = 0 }) => {
 
   return (
     <div>
-      {/* {actId !== 0 ? ( */}
-      <div>
-        <Formik
-          initialValues={data}
-          className="container-form"
-          onSubmit={(values) => handlerSubmit(values)}
-        >
-          {({ values, handleChange, handleSubmit }) => (
-            <form>
-              <Field
-                placeholder="Nombre de la actividad"
-                name="actname"
-                text="text"
-                onChange={handleChange}
-                value={values.content}
-              />
-              <label htmlFor="content">Descripcion de la actividad</label>
-              {actId !== 0 ? (
-                <Field
-                  placeholder
-                  name="content"
-                  as="textarea"
-                  onChange={handleChange}
-                  value={data.content}
-                />
-              ) : (
-                <CKEditor
-                  name="content"
-                  editor={ClassicEditor}
-                  data={values.content}
-                  onReady={(editor) => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log("Editor is ready to use!", editor);
-                  }}
-                  onChange={(editor) => {
-                    const data = editor?.getData();
-                    console.log(data);
-                    // setData({
-                    //   ...data,
-                    //   content: editor.getData(),
-                    // });
-                  }}
-                />
-              )}
-              <label htmlFor="file">Imagenes de las actividades</label>
-              <Field
-                name="file"
-                type="file"
-                multiple
-                onChange={handleChange}
-                value={values.file}
-              />
-              <button
-                type="submit"
-                className="button button-primary"
-                onClick={() => handlerSubmit(values)}
-              >
-                Enviar cambios
-              </button>
-            </form>
-          )}
-        </Formik>
-      </div>
-      {/* // ) : ( */}
-      {/* //   <div> */}
-      {/* <Formik */}
-      {/* //       initialValues={data} */}
-      {/* //       className="container-form" */}
-      {/* //       onSubmit={(values) => handlerSubmit(values)} */}
-      {/* //     > */}
-      {/* {({ values, handleChange, handlerSubmit }) => ( */}
-      {/* //         <form onSubmit={(values) => handlerSubmit(values)}> */}
-      {/* //           <label htmlFor="name">Nombre de la actividad</label> */}
-      {/* //           <Field */}
-      {/* //             name="name" */}
-      {/* //             text="text" */}
-      {/* //             onChange={handleChange} */}
-      {/* //             value={values.name} */}
-      {/* //           /> */}
-      {/* //           <label htmlFor="content">Descripcion de la actividad</label> */}
-      {/* //           image */}
-      {/* //           <label htmlFor="file">Imagenes de las actividades</label> */}
-      {/* //           <Field */}
-      {/* //             name="file" */}
-      {/* //             type="file" */}
-      {/* //             onChange={handleChange} */}
-      {/* //             value={values.file} */}
-      {/* //           /> */}
-      {/* //           <button */}
-      {/* //             type="submit" */}
-      {/* //             onClick={() => handlerSubmit(values)} */}
-      {/* //             className="button button-primary" */}
-      {/* //           > */}
-      {/* //             Enviar formulario */}
-      {/* //           </button> */}
-      {/* //         </form> */}
-      {/* //       )} */}
-      {/* //     </Formik> */}
-      {/* //   </div> */}
-      {/* // )} */}
+      <button className="modal-button" onClick={() => setVisible(!visible)}>
+        {actId !== 0 ? "Editar" : "Crear actividad"}
+      </button>
+      {visible && (
+        <div className="modal-activity">
+          <div
+            className="background-modal"
+            onClick={() => setVisible(!visible)}
+          ></div>
+          <Formik
+            initialValues={data}
+            onSubmit={(values) => handlerSubmit(values)}
+          >
+            {({ values, handleChange, handleSubmit }) => (
+              <form onSubmit={handleSubmit} className="container-form">
+                <div className="input-box">
+                  <Field
+                    placeholder="Nombre de la actividad"
+                    name="actname"
+                    text="text"
+                    value={values.actname}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="content">Descripcion de la actividad</label>
+                  {actId !== 0 ? (
+                    <CKEditor
+                      name="content"
+                      editor={ClassicEditor}
+                      data={values.content}
+                      onChange={handlerchange}
+                    />
+                  ) : (
+                    <CKEditor
+                      name="content"
+                      editor={ClassicEditor}
+                      data={values.content}
+                      onChange={handlerchange}
+                    />
+                  )}
+                </div>
+                <div className="input-box">
+                  <label htmlFor="file">Imagenes de las actividades</label>
+                  <Field
+                    name="file"
+                    type="file"
+                    onChange={handleChange}
+                    value={values.file}
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="button button-primary"
+                  onClick={() => handlerSubmit(values)}
+                >
+                  Enviar cambios
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      )}
     </div>
   );
 };
 
-EditActivities.propTypes = {};
+EditActivities.propTypes = {
+  actId: PropTypes.number.isRequired,
+};
 
 export default EditActivities;
