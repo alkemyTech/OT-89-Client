6682a9a5d966e0e59e129cfe12a4a1a20054a453
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2021-11-23 19:14:03 -0300

    Added transition logic to AuthRouter

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -3,13 +3,22 @@ import { Switch, Route } from "react-router-dom";
 import { Login } from "../pages/auth/Login";
 import { Register } from "../pages/auth/Register";
 import { BackOffice } from "../pages/backoffice/BackOffice";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./transitions.css"
 
 export const AuthRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={Login} />
-      <Route exact path="/auth/register" component={Register} />
-      <Route path="/auth/backoffice" component={BackOffice} />
-    </Switch>
+    <Route render={({ location }) => (
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+          <Switch location={location}>
+            <Route exact path="/auth/login" component={Login} />
+            <Route exact path="/auth/register" component={Register} />
+            <Route path="/auth/backoffice" component={BackOffice} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    )}
+    />
   );
 };
