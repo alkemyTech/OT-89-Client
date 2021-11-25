cce236782f2c337a665f5d54f56887d3307afb04
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-24 21:18:28 -0300

    What ONG does info added

--- src/pages/main/About.js
+++ src/pages/main/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AboutUs } from "../../components/AboutUs/AboutUs";
+import { WhatWeDo } from "../../components/AboutUs/WhatWeDo";
 import { Members } from "../../components/Members/Members";
 
 export const About = () => {
@@ -7,6 +8,7 @@ export const About = () => {
     <>
       <AboutUs />
       <Members />
+      <WhatWeDo />
     </>
   );
 };
