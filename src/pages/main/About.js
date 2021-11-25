6e3f0135bfb28eb6e1be3b161537ce584d54e426
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-24 21:01:41 -0300

    Information about ONG added

--- src/pages/main/About.js
+++ src/pages/main/About.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { AboutUs } from "../../components/AboutUs/AboutUs";
 import { Members } from "../../components/Members/Members";
 
 export const About = () => {
   return (
     <>
+      <AboutUs />
       <Members />
     </>
   );
